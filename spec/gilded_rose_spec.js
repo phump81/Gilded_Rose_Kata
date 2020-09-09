e85385233c2b331a8b124ab4b4df9b1c273532ac
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-09-09 11:17:12 +0100

    Tests for normal items

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,11 +1,23 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("normal items degrade by 1 after 1 day", function() {
-    const gildedRose = new Shop([ new Item("foo", 5, 5) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(4);
-    expect(items[0].sellIn).toEqual(4);
-  });
+    it("normal items degrade by 1 after 1 day", function() {
+      const gildedRose = new Shop([ new Item("foo", 5, 5) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(4);
+      expect(items[0].sellIn).toEqual(4);
+    });
+
+    it("once sellIn hits zero, quality degrades twice as fast", function() {
+      const gildedRose = new Shop([ new Item("foo", 0, 6) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(4);
+    })
+
+    it("the quality of an item is never negative", function() {
+      const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0);
+    })
 
 });
