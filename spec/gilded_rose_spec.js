de44b42c81dc3cb4f9e3c485c6e607251204a913
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-09-09 11:05:48 +0100

    Fixed first test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,11 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("normal items degrade by 1 after 1 day", function() {
+    const gildedRose = new Shop([ new Item("foo", 5, 5) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(4);
+    expect(items[0].sellIn).toEqual(4);
   });
 
 });
